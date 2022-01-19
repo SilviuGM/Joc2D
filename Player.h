6d6a9a045c24cb91ae37ab5c91a6106a974e916d
SilviuGM <80418019+SilviuGM@users.noreply.github.com>
2022-01-19 18:16:37 +0200

    Update Player.h

--- Player.h
+++ Player.h
@@ -8,16 +8,23 @@
 #include "Item.h"
 #include "User.h"
 #include <memory>
+#include <vector>
+
 class Player:public Entity {
 private:
 User Owner;
+std::vector<std::shared_ptr<Item>> Inventory;
 public:
+    Player() = default;
     Player(int x, int y, int hp, int atk, const std::string &name, const std::string &description, const User &owner);
     Player( const Player& copie );
 
+    void AddItem(std::shared_ptr<Item> item);
     bool operator==(const Player &rhs) const;
 
     bool operator!=(const Player &rhs) const;
+
+    std::ostream& print(std::ostream& os) const override;
 };
 
 
