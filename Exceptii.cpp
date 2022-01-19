9f8069c40f485aef1bb87a049a8313b7284df5c7
SilviuGM <80418019+SilviuGM@users.noreply.github.com>
2022-01-19 18:23:44 +0200

    Add files via upload

---
+++ Exceptii.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Silviu on 1/19/2022.
+//
+
+#include "Exceptii.h"
+
+Eroare_entity::Eroare_entity(const std::string &arg) : runtime_error("Eroare Entity:"+arg) {}
+
+Eroare_miscare::Eroare_miscare() : Eroare_entity("nu te poti deplasa mai mult de 100 miscari orizontal sau vertical") {}
---
+++ Exceptii.h
@@ -0,0 +1,18 @@
+//
+// Created by Silviu on 1/19/2022.
+//
+
+#ifndef PROIECTOOP_EXCEPTII_H
+#define PROIECTOOP_EXCEPTII_H
+#include <stdexcept>
+
+class Eroare_entity: public std::runtime_error {
+public:
+    Eroare_entity(const std::string &arg);
+};
+
+class Eroare_miscare: public Eroare_entity{
+public:
+    Eroare_miscare();
+};
+#endif //PROIECTOOP_EXCEPTII_H
