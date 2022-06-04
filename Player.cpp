d2be828a9732c399002a24fbbd47a9508d525f24
SilviuGM <80418019+SilviuGM@users.noreply.github.com>
2022-06-05 01:07:46 +0300

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -3,12 +3,16 @@
 //
 
 #include "Player.h"
+#include "Exceptii.h"
+#include "Entity.h"
+
 
 Player::Player(int x, int y, int hp, int atk, const std::string &name, const std::string &description,
                const User &owner) : Entity(x, y, hp, atk, name, description), Owner(owner) {}
 
-Player::Player(const Player &copie) {
-
+Player::Player(const Player &copy): Entity(copy) {
+    this->Owner = copy.Owner;
+    this->Inventory = copy.Inventory;
 }
 
 bool Player::operator==(const Player &rhs) const {
@@ -33,3 +37,27 @@ std::ostream &Player::print(std::ostream &os) const {
        os << "Item: \n" << *Item << "\n";
    }
 }
+void Player::MoveEntity(const Entity A, int x , int y) {
+    if(x>100 || y>100) throw Eroare_miscare();
+    this->x = A.getX()+x;
+    this->y= A.getY()+y;
+
+}
+
+void Player::AttackEntity(const Entity Attacker, const int n) {
+    if(n<0) throw Eroare_attack();
+    this->hp = this->hp-n*Attacker.getAtk();
+    if(this->hp<0) this->hp=0;
+
+}
+
+Player &Player::operator=(const Player &p) {
+if(this!=&p){
+   Entity::operator=(p);
+    this->Owner = p.Owner;
+    this->Inventory = p.Inventory;
+}
+    return *this;
+}
+
+
