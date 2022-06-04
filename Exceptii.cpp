9639c96251f08539346efba0735fc2bec12b8e19
SilviuGM <80418019+SilviuGM@users.noreply.github.com>
2022-06-05 01:02:12 +0300

    Update Exceptii.cpp

--- Exceptii.cpp
+++ Exceptii.cpp
@@ -7,3 +7,5 @@
 Eroare_entity::Eroare_entity(const std::string &arg) : runtime_error("Eroare Entity:"+arg) {}
 
 Eroare_miscare::Eroare_miscare() : Eroare_entity("nu te poti deplasa mai mult de 100 miscari orizontal sau vertical") {}
+
+Eroare_attack::Eroare_attack(): Eroare_entity("nu poti ataca cu o valoare negativa") {}
